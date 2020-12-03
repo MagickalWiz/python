c441191230ae746b13185349f8579ef1308b6301
MagickalWiz <54958629+MagickalWiz@users.noreply.github.com>
2020-12-03 11:33:09 -0800

    Add files via upload

---
+++ rasppi-py/scrotimg.py
@@ -0,0 +1,14 @@
+import os
+print("This python code will ask for a name for an image")
+print("then add a file name type of .png.")
+print("the *.png will be saved in the directory where this")
+print("file is executed.")
+imagename = input("Imput a name for an image : ")
+oscommand = "scrot "+imagename+".png"
+print(oscommand)
+os.system(oscommand)
+
+'''
+Now the challenge is to 
+os.system(sleep 5)
+'''
