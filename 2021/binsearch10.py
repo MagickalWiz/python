24cbf6e5671db8708395bb25ff84713e48216371
MagickalWiz <54958629+MagickalWiz@users.noreply.github.com>
2021-04-26 12:59:14 -0700

    Add files via upload

---
+++ 2021/binsearch10.py
@@ -0,0 +1,50 @@
+import random
+
+def main():
+	file = open("wordlist10.txt")
+	array = file.read().split(',')
+	
+	success = False
+	count = 1
+	word = ''
+	print("This program searches a list of 10-letter words to locate a randomized selection of letters.\n")
+	randomizeWord(array, success)
+	print("Place in array: " + str(success))
+
+
+
+def binsearchrand(array, w, success):
+	n = len(array)
+	s = 0
+	e = n
+	m = int((e + s) / 2)
+	
+	while(s <= e):
+		if(array[m] == w):
+			return m
+		else:
+			if(array[m] < w):
+				s = m + 1
+				m = int((e + s) / 2)
+			else:
+				e = m - 1
+				m = int((e + s) / 2)
+	print("not found\n")
+	return False
+
+
+def randomizeWord(array, success):
+	count = 0
+	while(success == False):
+		word = ''
+		for i in range(10):
+			rn = random.randint(97,122)
+			word = word + chr(rn)
+		print(word)
+		success = binsearchrand(array, word, success)
+		count = count + 1
+	print("Place in array: " + str(success) + " after " + str(count) + " loops")
+	
+
+if __name__ == '__main__':
+    main()
